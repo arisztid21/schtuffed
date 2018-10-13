6bf11995cbf3170025003ffb78fd634b1f34c82b
Parmesanio <sean_parmar@yahoo.com>
2018-10-13 14:56:36 -0700

    Initial Redux setup

---
+++ src/redux/restaurantReducer.js
@@ -0,0 +1,14 @@
+const initialState = {};
+
+
+//Action Types
+
+//Reducer Function
+export default function restaurantReducer(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return {...state}
+    }
+}
+
+//Action Creators
