dd29ab6fd5624845e502416e2d8588df774a3fde
Parmesanio <sean_parmar@yahoo.com>
2018-10-13 17:28:27 -0700

    Implemented GET restaurant list based on search inputs

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { setRestaurantList} from '../../redux/restaurantReducer';
+import { setRestaurantList, setCityId, setSearchInput } from '../../redux/restaurantReducer';
 
 class Homepage extends Component {
     componentDidMount() {
-        this.props.setRestaurantList();
+        // this.props.setRestaurantList();
     }
     render() { 
-        // let { setRestaurants } = this.props
+        let { setRestaurantList, setCityId, setSearchInput, searchInput, cityId } = this.props
         return ( 
             <React.Fragment>
                 <h1>Welcome to SCHTUFFED.COM this is APP.js</h1>
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <input onChange={(e) => setSearchInput(e.target.value)} type="text" placeholder="italian, burgers, vegetarian..." />
+                    <input onChange={(e) => setCityId(e.target.value)} type="text" placeholder="city or zip..." />
+                    <button onClick={() => setRestaurantList(searchInput, cityId)}>&#x1F50E;</button>
+                </form>
             </React.Fragment>
          );
     }
 }
  const mapStateToProps = state => {
-     let { restaurantList } = state.restaurants;
+     let { restaurantList, searchInput, cityId } = state.restaurants;
      return {
-         restaurantList
+         restaurantList,
+         searchInput,
+         cityId
      }
  }
-
-export default connect(mapStateToProps, {setRestaurantList})(Homepage);
+const mapDispatchToProps = {
+    setRestaurantList,
+    setCityId,
+    setSearchInput
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Homepage);
--- src/redux/restaurantReducer.js
+++ src/redux/restaurantReducer.js
@@ -1,29 +1,61 @@
 import axios from 'axios';
 
 const initialState = {
-    restaurantList: null
+    restaurantList: null,
+    cityId: null,
+    searchInput: null
 };
 
 
 //Action Types
-const SET_RESTAURANT_LIST = "SET_RESTAURANT_LIST";
+const SET_RESTAURANT_LIST = "SET_RESTAURANT_LIST",
+      SET_CITY_ID         = "SET_CITY_ID",
+      SET_SEARCH_INPUT    = "SET_SEARCH_INPUT";
 
 //Reducer Function
 export default function restaurantReducer(state = initialState, action) {
     switch(action.type) {
-        case SET_RESTAURANT_LIST:
+        case `${SET_RESTAURANT_LIST}_REJECTED`:
+            return {...state}
+        case `${SET_RESTAURANT_LIST}_PENDING`:
+            return {...state}
+        case `${SET_RESTAURANT_LIST}_FULFILLED`:
             return {...state, restaurantList: action.payload}
+        case `${SET_CITY_ID}_FULFILLED`:
+            return {...state, cityId: action.payload}
+        case SET_SEARCH_INPUT:
+            return {...state, searchInput: action.payload}
         default:
             return {...state}
     }
 }
 
 //Action Creators
-export function setRestaurantList() {
+
+//Set Restaurant List
+export function setRestaurantList(searchInput, cityId) {
     return {
         type: SET_RESTAURANT_LIST,
-        payload: axios.get('https://developers.zomato.com/api/v2.1/search?entity_type=city&count=10&sort=rating', {
+        payload: axios.get(`https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&q=${searchInput}&count=3&sort=rating`, {
             headers: {"user-key": "6ce2f2b32321de9306bcc12a5832bceb"}
-        }).then(res => console.log(res.data)).catch(err => console.log('Err in SET_RESTAURANT_LIST'))
+        }).then(res => res.data.restaurants).catch(err => console.log('Err in SET_RESTAURANT_LIST'))
+    }
+}
+
+//Set City Id
+export function setCityId(cityInput) {
+    return {
+        type: SET_CITY_ID,
+        payload: axios.get(`https://developers.zomato.com/api/v2.1/locations?query=${cityInput}`, {
+            headers: {"user-key": "6ce2f2b32321de9306bcc12a5832bceb"}
+        }).then(res => res.data.location_suggestions[0].city_id).catch(err => console.log('Err in SET_CITY_ID'))
+    }
+}
+
+//Set Search Input
+export function setSearchInput(val) {
+    return {
+        type: SET_SEARCH_INPUT,
+        payload: val
     }
 }
