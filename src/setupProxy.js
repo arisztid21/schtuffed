86569ec85331d25cfb42c23af9507e705b52bc05
Parmesanio <sean_parmar@yahoo.com>
2018-10-15 16:57:53 -0700

    Adjusted Proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,10 +6,3 @@ module.exports = function(app) {
     '/api/user-data'
     ],{ target: 'http://localhost:4000/' }));
 };
-
-// const proxy = require('http-proxy-middleware');
-
-// module.exports = function(app) {
-//     app.use(proxy( '/auth/callback',{ target: 'http://localhost:4000/' }));
-// };
-// /api/user-data
