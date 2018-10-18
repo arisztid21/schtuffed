a72392fc699947568c427b6b408c53b9e7123e40
jordanutz <jordanutz@outlook.com>
2018-10-18 11:14:19 -0700

    Single testimony

--- src/components/SingleTestimony/SingleTestimony.js
+++ src/components/SingleTestimony/SingleTestimony.js
@@ -3,23 +3,11 @@ import axios from 'axios'
 
 const SingleTestimony = (props) => {
 
-
-
-    console.log(props);
-<<<<<<< HEAD
+    console.log(props)
     return (
         <React.Fragment>
             Single Testi
             <button onClick={() => props.deleteTestimony(props.id)}>Delete Button</button>
-=======
-    let { title, ratings, description, date_posted } = props;
-    return ( 
-        <React.Fragment>
-            <h4>{title}</h4>
-            <h4>{date_posted}</h4>
-            <h4>{ratings}</h4>
-            <p>{description}</p>
->>>>>>> b075b1cf06e8bb494daf3302496ae33d551a991a
         </React.Fragment>
      );
 }
