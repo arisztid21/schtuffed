c9c57d8da91193012fbc8645d07d9dc7f5cffb21
Parmesanio <sean_parmar@yahoo.com>
2018-10-23 16:23:01 -0700

    Conditionally rendered headers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import routes from './routes';
+import { withRouter } from 'react-router-dom';
 import Header from './components/Header/Header';
 import SearchBar from './components/Header/SearchBarHeader';
 
 class App extends Component {
   render() {
+    console.log(this.props);
+    
     return (
       <div className="App">
-      <SearchBar />
-      <Header />
+      {this.props.location.pathname !== '/' ? <SearchBar /> : <Header /> }
+      
         {routes}
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
